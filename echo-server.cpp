33d1091661ee82989a2f21400f5265af431e04c9
Donghyun Lee <dominic2009@naver.com>
2021-06-13 00:43:00 +0900

    By gilgil's comments

--- echo-server.cpp
+++ echo-server.cpp
@@ -11,156 +11,168 @@
 #include <iostream>
 #include <thread>
 #include <set>
+#include <mutex>
 
 using namespace std;
 
-set<int> cli_sd_set;
+struct socket_list : set<int>
+{
+    mutex m;
+} cli_sd_set;
 
 #ifdef WIN32
 void perror(const char *msg)
 {
-	fprintf(stderr, "%s %ld\n", msg, GetLastError());
+    fprintf(stderr, "%s %ld\n", msg, GetLastError());
 }
 #endif // WIN32
 
 void usage()
 {
-	cout << "syntax : echo-server <port> [-e[-b]]\n";
-	cout << "sample : echo-server 1234 -e -b\n";
+    cout << "syntax : echo-server <port> [-e[-b]]\n";
+    cout << "sample : echo-server 1234 -e -b\n";
 }
 
 struct Param
 {
-	bool echo{false};
-	bool broadcast{false};
-	uint16_t port{0};
+    bool echo{false};
+    bool broadcast{false};
+    uint16_t port{0};
 
-	bool parse(int argc, char *argv[])
-	{
-		port = stoi(argv[1]);
-		for (int i = 2; i < argc; i++)
-		{
-			if (strcmp(argv[i], "-e") == 0)
-			{
-				echo = true;
-			}
-			else if (strcmp(argv[i], "-b") == 0)
-			{
-				broadcast = true;
-			}
-		}
-		return port != 0;
-	}
+    bool parse(int argc, char *argv[])
+    {
+        port = stoi(argv[1]);
+        for (int i = 2; i < argc; i++)
+        {
+            if (strcmp(argv[i], "-e") == 0)
+            {
+                echo = true;
+            }
+            else if (strcmp(argv[i], "-b") == 0)
+            {
+                broadcast = true;
+            }
+        }
+        return port != 0;
+    }
 } param;
 
 void recvThread(int sd)
 {
-	cout << "connected\n";
-	static const int BUFSIZE = 65536;
-	char buf[BUFSIZE];
-	while (true)
-	{
-		ssize_t res = recv(sd, buf, BUFSIZE - 1, 0);
-		if (res == 0 || res == -1)
-		{
-			cerr << "recv return " << res;
-			perror(" ");
-			break;
-		}
-		buf[res] = '\0';
-		cout << buf;
-		cout.flush();
-		if (param.echo)
-		{
-			res = send(sd, buf, res, 0);
-			if (res == 0 || res == -1)
-			{
-				cerr << "send return " << res;
-				perror(" ");
-				break;
-			}
-		}
-		if (param.broadcast)
-		{
-			for (int cli_sd : cli_sd_set)
-			{
-				res = send(cli_sd, buf, res, 0);
-				if (res == 0 || res == -1)
-				{
-					cerr << "send return " << res;
-					perror(" ");
-					break;
-				}
-			}
-		}
-	}
-	cout << "disconnected\n";
-	close(sd);
+    cout << "connected\n";
+    static const int BUFSIZE = 65536;
+    char buf[BUFSIZE];
+    while (true)
+    {
+        ssize_t res = recv(sd, buf, BUFSIZE - 1, 0);
+        if (res == 0 || res == -1)
+        {
+            cerr << "recv return " << res;
+            perror(" ");
+            break;
+        }
+        buf[res] = '\0';
+        cout << buf;
+        cout.flush();
+        if (param.echo)
+        {
+            res = send(sd, buf, res, 0);
+            if (res == 0 || res == -1)
+            {
+                cerr << "send return " << res;
+                perror(" ");
+                break;
+            }
+        }
+        if (param.broadcast)
+        {
+            lock_guard<mutex> lock(cli_sd_set.m);
+            for (int cli_sd : cli_sd_set)
+            {
+                res = send(cli_sd, buf, res, 0);
+                if (res == 0 || res == -1)
+                {
+                    cerr << "send return " << res;
+                    perror(" ");
+                    break;
+                }
+            }
+        }
+    }
+    cout << "disconnected\n";
+    {
+        lock_guard<mutex> lock(cli_sd_set.m);
+        cli_sd_set.erase(sd);
+    }
+    close(sd);
 }
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2 || argc > 4 || !param.parse(argc, argv))
-	{
-		usage();
-		return -1;
-	}
+    if (argc < 2 || argc > 4 || !param.parse(argc, argv))
+    {
+        usage();
+        return -1;
+    }
 
 #ifdef WIN32
-	WSAData wsaData;
-	WSAStartup(0x0202, &wsaData);
+    WSAData wsaData;
+    WSAStartup(0x0202, &wsaData);
 #endif // WIN32
 
-	int sd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sd == -1)
-	{
-		perror("socket");
-		return -1;
-	}
+    int sd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sd == -1)
+    {
+        perror("socket");
+        return -1;
+    }
 
-	int res;
+    int res;
 #ifdef __linux__
-	int optval = 1;
-	res = setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
-	if (res == -1)
-	{
-		perror("setsockopt");
-		return -1;
-	}
+    int optval = 1;
+    res = setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+    if (res == -1)
+    {
+        perror("setsockopt");
+        return -1;
+    }
 #endif // __linux
 
-	struct sockaddr_in addr;
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_port = htons(param.port);
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(param.port);
 
-	ssize_t res2 = ::bind(sd, (struct sockaddr *)&addr, sizeof(addr));
-	if (res2 == -1)
-	{
-		perror("bind");
-		return -1;
-	}
+    ssize_t res2 = ::bind(sd, (struct sockaddr *)&addr, sizeof(addr));
+    if (res2 == -1)
+    {
+        perror("bind");
+        return -1;
+    }
 
-	res = listen(sd, 5);
-	if (res == -1)
-	{
-		perror("listen");
-		return -1;
-	}
+    res = listen(sd, 5);
+    if (res == -1)
+    {
+        perror("listen");
+        return -1;
+    }
 
-	while (true)
-	{
-		struct sockaddr_in cli_addr;
-		socklen_t len = sizeof(cli_addr);
-		int cli_sd = accept(sd, (struct sockaddr *)&cli_addr, &len);
-		if (cli_sd == -1)
-		{
-			perror("accept");
-			break;
-		}
-		cli_sd_set.insert(cli_sd);
-		thread *t = new thread(recvThread, cli_sd);
-		t->detach();
-	}
-	close(sd);
+    while (true)
+    {
+        sockaddr_in cli_addr;
+        socklen_t len = sizeof(cli_addr);
+        int cli_sd = accept(sd, (struct sockaddr *)&cli_addr, &len);
+        if (cli_sd == -1)
+        {
+            perror("accept");
+            break;
+        }
+        {
+            lock_guard<mutex> lock(cli_sd_set.m);
+            cli_sd_set.insert(cli_sd);
+        }
+        thread *t = new thread(recvThread, cli_sd);
+        t->detach();
+    }
+    close(sd);
 }
